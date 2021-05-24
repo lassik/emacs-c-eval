fe46ae2204c49946364d68203d9f4281b62f398e
Lassi Kortela <lassi@lassi.io>
2021-05-25 00:21:51 +0300

    Update commentary

--- c-eval.el
+++ c-eval.el
@@ -14,16 +14,20 @@
 ;;; Commentary:
 
 ;; By analogy with the Lisp Interaction mode that comes with Emacs,
-;; this package provides the following commands to instantly compile
-;; and run C code:
-
-;; * `c-eval-buffer'
-;; * `c-eval-region'
-;; * `c-eval-expression'
-;; * `c-eval-sizeof'
+;; this package provides commands to instantly compile and run C code.
 
 ;; Use `c-eval-scratch' to get a buffer in which to write code. You
 ;; can also evaluate code from other buffers.
+
+;; The following commands execute a C program from the current buffer:
+
+;; * `c-eval-buffer'
+;; * `c-eval-region'
+
+;; The following commands evaluate C code from the minibuffer:
+
+;; * `c-eval-expression'
+;; * `c-eval-sizeof'
 
 ;;; Code:
 
